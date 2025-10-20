68e5ce2fd6d82841b754f69ab4a14e82f38eae6e
mypotato20-ops <mypotato20@gmail.com>
2025-10-20 15:47:22 +0900

    Colab을 통해 생성됨

---
+++ 20일(월)연습_py의_사본.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPRMVDkLY9sLEbjuDfTMA0x",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/mypotato20-ops/income_tax.py/blob/main/20%EC%9D%BC(%EC%9B%94)%EC%97%B0%EC%8A%B5_py%EC%9D%98_%EC%82%AC%EB%B3%B8.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "uuCSVqlAXdVa"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
